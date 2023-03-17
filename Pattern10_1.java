81a294cee8c2616a3e2ba1f715598f6a11e2278f
Yashmule04 <54475652+Yashmule04@users.noreply.github.com>
2023-03-17 17:43:45 +0530

    Add files via upload

---
+++ Pattern10_1.java
@@ -0,0 +1,16 @@
+class Pattern10_1{
+	public static void main(String args[]){
+		for(char i='E'; i>='A';i--)
+		{
+			for(char j='A';j<=i-1;j++)
+			{
+				System.out.print(" ");
+			}
+			for(char k=i;k<='E';k++)
+			{
+				System.out.print(k+" ");
+			}
+			System.out.println();
+		}
+	}
+}
